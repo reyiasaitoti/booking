d33d9bbbf9951b1a243680c7f4b5603a9045a2c9
reyiasaitoti <reyiabenjamin8@gmail.com>
2025-05-20 23:52:59 +0300

    help

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ const PORT = process.env.PORT || 5000; // use PORT from Render or fallback to 50
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Middleware
-app.use(cors());
+app.use(cors({
+   origin: 'https://fro-booking.vercel.app', // Replace with your actual frontend domain
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
